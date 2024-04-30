556d8678f4901ad431e04a8102077771bcab5688
RabbanMehamood <shaik.mehamood786@gmail.com>
2024-04-30 05:22:50 +0000

    initial commit

---
+++ src/components/HistoryItem/index.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import './index.css'
+
+const HistoryItem = props => {
+  const {historyItem, deleteItem} = props
+  const {id, timeAccessed, logoUrl, title, domainUrl} = historyItem
+
+  const handleDelete = () => {
+    deleteItem(id)
+  }
+  return (
+    <li className="list-style">
+      <div className="list-style">
+        <p>{timeAccessed}</p>
+        <img src={logoUrl} alt="domain logo" />
+        <p>{title}</p>
+        <p>{domainUrl}</p>
+      </div>
+      <div>
+        <button type="button" onClick={handleDelete}>
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/delete-img.png"
+            alt="delete"
+          />
+        </button>
+      </div>
+    </li>
+  )
+}
+export default HistoryItem
